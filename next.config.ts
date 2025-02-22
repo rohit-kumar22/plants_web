569aca72f661d2fca5cd55a1136555a8fc95c11c
rohit-kumar22 <rohitpal8958@gmail.com>
2025-02-22 10:33:27 +0530

    repo name

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export", // Enables static export
-  basePath: isProd ? "https://github.com/rohit-kumar22/plants_web.git" : "", // Change 'your-repo-name' to your actual repo name
+  basePath: isProd ? "/plants_web" : "", // Change 'your-repo-name' to your actual repo name
   images: {
     unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
   },
